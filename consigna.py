b53fe940f08d081b09912dd44feee69326495bfa
PanchoF1 <fsadams@hotmail.com>
2024-04-23 22:07:35 -0300

    agregados tests

--- consigna.py
+++ consigna.py
@@ -1,3 +1,4 @@
+import unittest
 def buscar_datos(*args, **kwargs):
     for key, person_data in database.items():
         if all(arg in person_data.values() for arg in args):
@@ -23,5 +24,32 @@ database = {
             }
 }
 
-resultado = buscar_datos("Pablo", "Diego", "Ruiz", "Picasso", **database)
-print(resultado)
+#resultado = buscar_datos("Pablo", "Diego", "Ruiz", "Picasso", **database)
+#print(resultado)
+
+class TestBuscarDatos(unittest.TestCase):
+    def test_pablo(self):
+        resultado = buscar_datos("Pablo","Diego","Ruiz","Picasso",**database)
+        self.assertEqual(resultado, "1")
+    def test_elio(self):
+        resultado = buscar_datos("Elio", "Anci", **database)
+        self.assertEqual(resultado, "2")
+
+    def test_elias(self):
+        resultado = buscar_datos("Elias", "Marcos", "Luciano", "Marcelo", "Gonzalez", **database)
+        self.assertEqual(resultado, "3")
+
+    def test_nn(self):
+        resultado = buscar_datos("Fernando", "Alonso", "14", **database)
+        self.assertIsNone(resultado)
+
+    def test_nn2(self):
+        resultado = buscar_datos("George", "Russell", "Perez", **database)
+        self.assertIsNone(resultado)
+
+    def test_nn3(self):
+        resultado = buscar_datos("Lando", "Mercedes", "Norris", "2", **database)
+        self.assertIsNone(resultado)
+    
+if __name__ == '__main__':
+    unittest.main()
